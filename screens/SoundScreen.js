ddcc36b05cc27ccf200deb49a87713fd38506cdb
dnirns <daniel.irons@gmail.com>
2020-12-18 22:28:22 +0000

    added expo-av and linked up to audio file

--- screens/SoundScreen.js
+++ screens/SoundScreen.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { Audio } from 'expo-av'
+
+
+
+const balloon = '../assets/mp3/Balloon-Pop.mp3'
 
 const SoundScreen = () => {
+  const [sound, setSound] = useState()
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(require(balloon))
+    setSound(sound)
+    await sound.playAsync()
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound')
+          sound.unloadAsync()
+        }
+      : undefined
+  }, [sound])
+
   return (
     <View>
       <Text>SOUND BOARD GOES HERE</Text>
+      <Button title='POP BALLOON' onPress={playSound} />
     </View>
   )
 }
